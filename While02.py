fcf26398ed92fc55e3bd6b1a6b1a8cd62a2a75dc
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-26 14:55:45 +0500

    bajarildi

--- While02.py
+++ While02.py
@@ -6,4 +6,6 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    s='python2023'
+    return s.count('a')+s.count('b')+s.count('c')+s.count('d')+s.count('e')+s.count('f')+s.count('g')+s.count('h')+s.count('i')+s.count('j')+s.count('k')+s.count('l')+s.count('m')+s.count('n')+s.count('o')+s.count('p')+s.count('q')+s.count('r')+s.count('s')+s.count('t')+s.count('u')+s.count('v')+s.count('w')+s.count('x')+s.count('y')+s.count('z')
+print(main('python2023'))
